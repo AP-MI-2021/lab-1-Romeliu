e33fe46d34b734e602aec91fe9ba59d2e7dc98b8
Romeliu Bura <romeliu.bura@gmail.com>
2021-09-29 12:02:38 +0300

    primul commit

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-'''
-Returneaza true daca n este prim si false daca nu.
-'''
+# '''
+# Returneaza true daca n este prim si false daca nu.
+# '''
 def is_prime(n):
   # codul vostru aici
   
   
-'''
-Returneaza produsul numerelor din lista lst.
-'''
+# '''
+# Returneaza produsul numerelor din lista lst.
+# '''
 def get_product(lst):
   # codul vostru aici
   
   
-'''
-Returneaza CMMDC a doua numere x si y folosind primul algoritm.
-'''
+# '''
+# Returneaza CMMDC a doua numere x si y folosind primul algoritm.
+# '''
 def get_cmmdc_v1(x, y):
   # codul vostru aici
   
+
+# comment
   
-'''
-Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
-'''
+# '''
+# Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
+# '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
   
